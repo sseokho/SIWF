d9b0f4e9ec29619f7e1aa1126935371f8c6bf9a0
son seokho <pub@wordncode.com>
2024-05-16 18:17:38 +0900

    pub commit

--- resource/js/include.js
+++ resource/js/include.js
@@ -2,6 +2,3 @@
 $('.header').load('././include/header.html')
 $('.footer').load('././include/footer.html')
 
-
-$('.sub_header').load('../../include/sub_header.html')
-$('.sub_footer').load('../../include/sub_footer.html')
--- resource/js/sub.js
+++ resource/js/sub.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     liveSwiper();
     accordion();
     td_file();
+    hisProgram();
 })
 
 function bradcrumb(){
@@ -26,9 +27,9 @@ function bradcrumb(){
             $(".depth_1 > li > a").removeClass('active');
             $(this).addClass('active');
         })
-        
 
-        
+
+
         var aa = $(".depth1_a");
         var aaClone = aa.clone();
 
@@ -36,30 +37,30 @@ function bradcrumb(){
 
         aa.each((i,v) => {
 
-            
-            
+
+
             var bradIsActive = $(".bradcrumb__tit.is-active").text();
 
-            
+
 
             var depth1a = $(".depth1_a");
-            
+
             var depth1aText = depth1a.eq(i).text();
-            
-            
 
 
 
-            
+
+
+
 
             if(bradIsActive == depth1aText){
                 depth1a.eq(i).addClass("include");
             }
-            
+
         });
 
-        
-   
+
+
 
         var next = $(".include").next().children("li").find('a');
         var nextCopy = next.clone();
@@ -68,20 +69,20 @@ function bradcrumb(){
 
         /*
         next.each((i,v)=> {
-            
-            
+
+
             console.log("ff",v);
-            
+
         })
         */
-        
-        
-        
+
+
+
 
 
     });
 
-    
+
 
 
 
@@ -91,8 +92,8 @@ function bradcrumb(){
         $(this).parent().siblings().children('.bradcrumb__links').slideUp();
     })
 
-    
-    
+
+
 }
 
 
@@ -101,7 +102,7 @@ function bradcrumb(){
 
 function liveSwiper() {
     var liveSwiper = new Swiper('.liveSwiper', {
-     
+
 
 
         slidesPerView: 'auto',
@@ -127,45 +128,116 @@ function liveSwiper() {
           liveSwiper.autoplay.stop();
         }
     });
-         
-     
+
+
   }
 
 function accordion() {
-    $(".accordion-wrap__item.fix").addClass('on');
-
-    $('.accordion-wrap__item').hover(function () {
-        if ($(this).hasClass('on')) {
-          $(this).removeClass('on');
-        } else {
-          $(this).addClass('on');
-        }
-
-        if ($(".accordion-wrap__item").hasClass('on')) {
-            $(".accordion-wrap__item.fix").removeClass('on');
-        } else{
+    
+    $(window).resize(function() {
+        $(".accordion-wrap__item").removeClass('on');
+        if (window.innerWidth < 1024) {  // 다바이스 크기가 480이상일때
             $(".accordion-wrap__item.fix").addClass('on');
+            $('.accordion-wrap__item').click(function () {
+                $('.accordion-wrap__item').not($(this)).removeClass("on");
+                $(this).addClass("on");
+
+            });
+
+        } else {
+            $(".accordion-wrap__item.fix").addClass('on');
+            $('.accordion-wrap__item').hover(function () {
+                if ($(this).hasClass('on')) {
+                  $(this).removeClass('on');
+                } else {
+                  $(this).addClass('on');
+                }
+
+                if ($(".accordion-wrap__item").hasClass('on')) {
+                    $(".accordion-wrap__item.fix").removeClass('on');
+                } else{
+                    $(".accordion-wrap__item.fix").addClass('on');
+                }
+            });
         }
+        
 
-
-    });
+    }).resize();
 }
 
 function td_file(){
     var td_file = $(".td-file")
-    
+
     td_file.each((i,v) => {
-        
-        
+
+
         if(td_file.eq(i).find("a").length){
             td_file.eq(i).parent('tr').removeClass("noFIle");
         } else{
             td_file.eq(i).parent('tr').addClass("noFile");
-            
+
         }
 
 
     })
-    
-    
+
+
+}
+
+function hisProgram(){
+
+    $(".slider").each(function(index){
+        let $this = $(this);
+        let swiper = undefined;
+        let slideNum =  $this.find('.swiper-slide').length //슬라이드 총 개수
+        let slideInx = 0; //현재 슬라이드 index
+
+        //디바이스 체크
+        let oldWChk = window.innerWidth > 768 ? 'pc' : 'mo';
+        sliderAct();
+        $(window).on('resize', function () {
+            let newWChk = window.innerWidth > 768 ? 'pc' : 'mo';
+            if(newWChk != oldWChk){
+                oldWChk = newWChk;
+                sliderAct();
+            }
+        })
+
+        function sliderAct(){
+            //슬라이드 인덱스 클래스 추가
+            $this.addClass(`slider${index}`);
+
+            //슬라이드 초기화
+            if (swiper != undefined){
+                swiper.destroy();
+                swiper = undefined;
+            }
+
+            //slidesPerView 옵션 설정
+            let viewNum = oldWChk == 'pc' ? 5 : 2;
+            //loop 옵션 체크
+            let loopChk = slideNum > viewNum;
+
+            swiper = new Swiper(`.slider${index} .inner`, {
+                slidesPerView: viewNum,
+                initialSlide :slideInx,
+                spaceBetween: 0,
+                slidesPerView: 1,
+                loop: loopChk,
+                pagination: {
+                    el: $(`.slider${index} .swiper-pagination`)[0],
+                },
+                on: {
+                    activeIndexChange: function () {
+                        slideInx = this.realIndex; //현재 슬라이드 index 갱신
+                    }
+                },
+            });
+        }
+    });
+
+
+
+
+
 }
